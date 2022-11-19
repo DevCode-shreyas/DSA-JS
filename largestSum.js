c024c158ef52c7a539d22ffff9432f9a01da37e3
shreyas <shreyasbole4@gmail.com>
2022-11-19 17:38:26 +0530

    day4

---
+++ largestSum.js
@@ -0,0 +1,21 @@
+// count largest sum of consecutive digits
+function findLargest(array, num) {
+  if (num > array) {
+    throw new Error("num is not greater than array");
+  } else {
+    let max = 0;
+    for (let i = 0; i < array.length - num + 1; i++) {
+      let temp = 0;
+      for (let j = 0; j < num; j++) {
+        temp += array[i + j];
+      }
+      if (temp > max) {
+        max = temp;
+      }
+    }
+    return max;
+  }
+}
+
+const result = findLargest([1, 2, 3, 4, 2, 5, 4, 6, 7, 8], 3);
+console.log(result);
