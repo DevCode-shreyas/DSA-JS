6f3a511ee99214252a0af0dff1e74cd20f11ee7f
shreyas <shreyasbole4@gmail.com>
2022-11-27 18:17:51 +0530

    day12

---
+++ insertionSort.js
@@ -0,0 +1,17 @@
+const arr = [7, 8, 2, 4, 1, 3];
+
+const sorted = (arr) => {
+  for (let i = 1; i < arr.length; i++) {
+    let cur = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > cur) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = cur;
+  }
+  return arr;
+};
+
+const res = sorted(arr);
+console.log(res);
